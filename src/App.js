8833b251fd8f464430ca5838360b96a0c108c2f1
Christopher agüero <chris19986@gmail.com>
2022-10-24 16:59:50 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import './App.css';
 import Header from './Header';
-import Quote from './Quote';
-import Button from './Button';
 import GetQuote from './GetQuote';
 
 function App() {
   return (
     <div className="container">
-          {/*
-          header component
-          quote component
-          button component
-          getQoute component
-          */}
           <Header/>
-          <Quote/>
-          <Button/>
           <GetQuote/>
+          
     </div>
   );
 }
--- src/GetQuote.js
+++ src/GetQuote.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import { useEffect,useState } from 'react';
+import Button from './Button';
 
 export default function GetQuote() {
-  
+  // fetching
   function loadQuote(){
     fetch('	https://api.adviceslip.com/advice')
       .then((response) => response.json())
       .then((data) => setAdvice(data.slip.advice));
   }
-  
-  const [advice, setAdvice] = useState([])
-  useEffect(()=> {loadQuote()}, [])
+  // useState(hook) stores the data coming from the fetching
+  const [advice, setAdvice] = useState([]) // empty array is the defualt state
+
+  // useEffect(hook) is for make a request and update the page
+  useEffect(()=> {loadQuote()}, [])// empty array => dependency array
  
   return (
     <div>
-      <h1> {advice} </h1>
+      <h1> {advice} </h1>{/*advice variable refers to whatever that is in that state*/}
+      <Button onClick={loadQuote}/>{/* no funciona asi */}
     </div>
   )
 }
